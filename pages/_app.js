ff04601e8fdb59ffab33f3a6be480a7cfff296ad
Spenser Wellen <spenserwellen@gmail.com>
2021-06-29 20:06:58 -0700

    removed header / footer from login & signup pages! :)

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,23 @@
 import '../styles/globals.css';
 import Layout from '../src/components/layout';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  const router = useRouter();
+
+  if (router.pathname === '/login' || router.pathname === '/signup') {
+    return (
+      <>
+        <Component {...pageProps} />
+      </>
+    );
+  } else {
+    return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    );
+  }
 }
 
 export default MyApp;
